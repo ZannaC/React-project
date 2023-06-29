786f7eb805deb19332ea52eebb5253e7ff53babc
ZannaC <zhanna_s1@inbox.lv>
2023-06-29 08:53:07 +0300

    Add useState practice

--- starting-project/src/components/Expenses/ExpenseItem.js
+++ starting-project/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
@@ -30,3 +38,34 @@ export default ExpenseItem;
 //     </div>
 //   );
 // }
+
+/// Exercise1: Listening to Events ///
+/*import React from 'react';
+
+import './styles.css';
+export default function App() {
+    const clickHandler = () => {
+    console.log("Stored!");
+  };
+    return <button onClick={clickHandler}>Bookmark</button>;
+}*/
+
+/// Exercise2: Working with "State" ///
+/* import React, {useState} from 'react';
+import './styles.css';
+
+export default function App() { 
+
+    const [price, setPrice] = React.useState('100');
+
+  const clickHandler = () => {
+    setPrice('75');
+    console.log(price);
+  };
+    return (
+        <div>
+            <p>${price}</p>
+            <button onClick={clickHandler}>Apply Discount</button>
+        </div>
+    );
+} */
