1ca0c7ec1d0fb9551fc890d7a5d073dbc3ae9bdc
ZannaC <zhanna_s1@inbox.lv>
2023-06-29 14:43:18 +0300

    Add multiple States practice

--- starting-project/src/App.js
+++ starting-project/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 // Arrow function
@@ -36,7 +37,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
