17a6c80fc2202fcd9dd47a1c9788f0ae2f3bbcf6
ZannaC <zhanna_s1@inbox.lv>
2023-07-01 14:45:40 +0300

    Add changes

--- exercise/src/App.js
+++ exercise/src/App.js
@@ -1,20 +1,42 @@
 import React, { useState } from "react";
 import "./index.css";
+import Price from "./Price";
+import Counter from "./Counter";
 
 function App() {
   const [inputMessage, setInputMessage] = useState(" ");
+  const [price, setPrice] = useState("100");
+  const [counter, setCounter] = useState(0);
+
   const changeMessage = (event) => {
     setInputMessage(event.target.value);
   };
+  const applyDiscount = () => {
+    setPrice("75");
+  };
+  const incrementCounter = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
   return (
     <form>
-      <label>Your message</label>
-      <input onChange={changeMessage} type="text" />
-      <p>
-        {inputMessage.trim().length < 3
-          ? "Invalid message🙄"
-          : "Valid message😉👍"}
-      </p>
+      <div>
+        <label>Your message</label>
+        <input onChange={changeMessage} type="text" />
+        <p>
+          {inputMessage.trim().length < 3
+            ? "Invalid message🙄"
+            : "Valid message😉👍"}
+        </p>
+      </div>
+
+      <div>
+        <Price price={price} applyDiscount={applyDiscount} />
+      </div>
+
+      <div>
+        <Counter counter={counter} incrementCounter={incrementCounter} />
+      </div>
     </form>
   );
 }
