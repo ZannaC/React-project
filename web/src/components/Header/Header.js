92fec9ca05b9312b15f003673e1c311e75a8f380
ZannaC <zhanna_s1@inbox.lv>
2023-07-24 13:36:30 +0300

    Add investment-calculator project start files

--- web/src/components/Header/Header.js
+++ web/src/components/Header/Header.js
@@ -1,12 +1,25 @@
+import React, { useState } from "react";
 import keyConceptsImage from "../../assets/images/key-concepts.png";
+import "../../index.css";
 
-function Header() {
+const Header = (props) => {
+  const [isHighlighted, setIsHighlighted] = useState(false);
+  const imgBack = isHighlighted ? "cyan" : "rgba(234, 78, 240, 0.4)";
+
+  const toggleStylingHandler = () =>
+    setIsHighlighted((wasHighlighted) => !wasHighlighted);
+
   return (
     <header>
-      <img src={keyConceptsImage} alt="Medal badge with a star" />
+      <img
+        onClick={toggleStylingHandler}
+        style={{ backgroundColor: imgBack }}
+        src={keyConceptsImage}
+        alt="Medal badge with a star"
+      />
       <h1>Key React Concepts</h1>
       <p>Selected key React concepts you should know about</p>
     </header>
   );
-}
+};
 export default Header;
